aced1d0dde20e85ae180f13b6b30291571884c29
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-09-20 13:10:12 +0330

    Add project files.

---
+++ CSV_To_SQL/CSVToSQLForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CSV_To_SQL
+{
+    partial class CSVToSQLForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(97, 69);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(250, 125);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // CSVToSQLForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "CSVToSQLForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MMK - CSV to SQL Statements Converter";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+    }
+}
---
+++ CSV_To_SQL/CSVToSQLForm.cs
@@ -0,0 +1,10 @@
+namespace CSV_To_SQL
+{
+    public partial class CSVToSQLForm : Form
+    {
+        public CSVToSQLForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
